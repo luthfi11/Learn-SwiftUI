c2dcbbca1ee0a587fc89586bb744ad271b6b8425
Luthfi Alfarisi <luthfi.alfarisi@dansmultipro.com>
2021-06-27 21:07:25 +0700

    Initial Commit

---
+++ PremierLeague/PremierLeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  PremierLeagueApp.swift
+//  PremierLeague
+//
+//  Created by Dans Multipro on 27/06/21.
+//
+
+import SwiftUI
+
+@main
+struct PremierLeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
